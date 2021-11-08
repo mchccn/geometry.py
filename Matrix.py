6ee630e0e68c23a4cd51097a3433d5b028ba8c11
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-11-08 16:55:26 -0600

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -71,14 +71,111 @@ class Matrix:
     
     result = 0
     
-    for x in range(len(width)):
-      sub =  [row[:x] + row[x + 1:] for row in (m[:0] + m[1:])]
+    for x in range(len(self.width)):
+      sub = [row[:x] + row[x + 1:] for row in (m[:0] + m[1:])]
       
-      result += (-1) ** (x + 2) * self.contents[0][x] * Matrix.from_array(sub).determinant()
+      result += (-1) ** (x + 2) * self.contents[0][x] * Matrix.from_list(sub).determinant()
     
     return result
   
-  # cont'd
+  @property
+  def minors(self):
+    minors = Matrix(self.width, self.height)
+    
+    for y in range(len(self.height)):
+      for x in range(len(self.width)):
+        minors.set(x, y, Matrix.from_list([row[:x] + row[x + 1:] for row in (m[:y] + m[y + 1:])]).determinant())
+        
+    return minors
+  
+  @property
+  def cofactors(self):
+    checkerboard = [[1] * width] * height
+    
+    for y in range(len(self.height)):
+      for x in range(len(self.width)): 
+        if (x + y) % 2 != 0:
+          checkerboard[y][x] = -1
+          
+    minors = self.minors
+    
+    cofactors = [[0] * width] * height
+    
+    for y in range(len(self.height)):
+      for x in range(len(self.width)): 
+        cofactors[y][x] = minors[y][x] * checkerboard[y][x]
+    
+    return cofactors
+  
+  @property
+  def adjugate(self):
+    return self.cofactors.transpose()
+  
+  @property
+  def inverse(self):
+    determinant = self.determinant()
+    
+    if determinant == 0:
+      raise ValueError
+      
+    adjugate = this.adjugate
+    
+    return Matrix.from_list(list(map(lambda row: list(map(lambda x: (1 / determinant) * x, row)), self.contents)))
+  
+  def to_list(self):
+    return self.contents
+  
+  def __str__(self):
+    pass
+  
+  def clone(self):
+    return Matrix(self.width, self.height, self.contents)
+  
+  def rotate(self, dir):
+    if dir < 0:
+      l = list(zip(*self.contents[::-1]))
+      self.contents = [list(r) for r in l]
+    elif dir > 0:
+      l = list(zip(*self.contents))[::-1]
+      self.contents = [list(r) for r in l]
+    
+    return self
+  
+  def transpose(self):
+    self.reverse()
+    
+    self.rotate(1)
+    
+    return this
+  
+  @staticmethod
+  def rotate(m):
+    return m.clone().rotate()
+  
+  @staticmethod
+  def zeroes(self, width, height):
+    return Matrix(width, height, [[0] * width] * height)
+  
+  @staticmethod
+  def ones(self, width, height):
+    return Matrix(width, height, [[1] * width] * height)
+  
+  @staticmethod
+  def identity(s):
+    identity = Matrix.zeroes(s, s)
+    
+    for y in range(s):
+      for x in range(s):
+        if x == y:
+          identity[y][x] = 1
+          
+    return identity
+  
+  @staticmethod
+  def from_list(a):
+    return Matrix(a.length, a[0].length, a)
+   
+  # 
     
   @staticmethod
   def is_matrix(v):
